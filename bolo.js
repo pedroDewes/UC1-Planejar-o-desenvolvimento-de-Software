fdade218d888fa889d1f8cd79ee496b131b068f7
pedroDewes <pedrolimadewes@gmail.com>
2025-09-01 21:55:33 -0300

    Add files via upload

---
+++ bolo.js
@@ -0,0 +1,27 @@
+//fazendo bolo
+primeiroPasso = "No liquidificador bata a  água, o Neskquick, a manteiga, os ovos, o acucar ate ficar homogeneo \n"
+segundoPasso = "Transfire a mistura para uma tigela \n"
+terceiroPasso = "Adicione a farinha e o fermento \n"
+quartoPasso = "Misture com uma colher \n"
+sextoPasso = "Despeje a mistura em uma forma de buraco \n"
+quintoPasso = "Enfarinhe a forma de buraco \n"
+setimoPasso =  "Leve ao forno pre aquecido por 35 minutos \n"
+oitavoPasso = "Retire e deixe esfriar \n"
+nonoPasso = "Ao esfriar, desinforme e corte a massa na horizontal em 3 pedacos iguais \n"
+decimoPasso = "Reserve a massa \n"
+decimo1Passo = "Para a cobertura, pegue uma penela \n"
+decimo2Passo = "Na penela misture o morango, o acucar e o suco de limao \n"
+decimo3Passo = "Leve a mistura ao fogo baixo e deixe levantar fervura \n"
+decimo4Passo = "Cozinhe por 5 minutos e deixe esfriar \n"
+decimo5Passo = "Acrescente 1/3 da cobertura na superficie do primeiro pedaco da massa \n"
+decimo6Passo = "Coloque o segundo pedaco da massa em cima da cobertura do primeiro pedaco \n"
+decimo7Passo = "Acrescente metade do restante da cobertura em cima do segundo pedaco de massa \n"
+decimo8Passo = "Coloque o terceiro pedaco de massa em cima da cobertura do segundo pedaco de massa \n"
+decimo9Passo = "Acrescente o restante da cobertura sobre o bolo \n"
+vigesimoPasso = "Acrescente morongos frescos sobre a cobertura do bolo \n"
+vigesimo1Passo = "Despeje leite condensado em formas horizontais na superficie do bolo \n"
+vigesimo2Passo = "Sirva \n"
+
+// utilizando o console.log para exibir informações
+console.log(primeiroPasso, segundoPasso, terceiroPasso, quintoPasso, sextoPasso, setimoPasso, oitavoPasso, nonoPasso, decimoPasso, decimo1Passo, decimo2Passo, decimo3Passo, decimo4Passo, decimo5Passo, decimo6Passo, decimo7Passo, decimo8Passo, decimo9Passo, vigesimoPasso, vigesimo1Passo, vigesimo2Passo)
+ 
